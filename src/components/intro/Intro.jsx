65d451248bdfb90642c744c8d4c2aec10a01bb0d
Shivam Sengar <shivamsengar995627@gmail.com>
2024-01-08 11:18:16 +0530

    Added Intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,38 @@
-import "./intro.scss"
-
+import "./intro.scss";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 const Intro = () => {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay:1500,
+      backSpeed:60,
+      strings: ["Developer"],
+    });
+  }, []);
   return (
-    <div className="intro" id="intro">Intro</div>
-  )
-}
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="my-image">
+          <img src="/src/assets/my_image.jpeg" alt="my-image" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Shivam Sengar</h1>
+          <h3>
+            React <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <KeyboardArrowDownIcon className="arrow-down" />
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Intro
+export default Intro;
